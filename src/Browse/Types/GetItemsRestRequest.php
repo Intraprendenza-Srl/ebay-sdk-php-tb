c58b829e1b740cd4489d479d3944f56b45bf51e5
Davide Pilato <de.pilato@gmail.com>
2025-01-17 16:30:39 +0100

    fix psr

--- src/Browse/Types/GetItemsRestRequest.php
+++ src/Browse/Types/GetItemsRestRequest.php
@@ -21,14 +21,11 @@ class GetItemsRestRequest extends \DTS\eBaySDK\Types\BaseType
 	 */
 	public function __construct(array $values = [])
 	{
-		list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
-
+		[$parentValues, $childValues] = self::getParentValues(self::$propertyTypes, $values);
 		parent::__construct($parentValues);
-
 		if (!array_key_exists(__CLASS__, self::$properties)) {
 			self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
 		}
-
 		$this->setValues(__CLASS__, $childValues);
 	}
 }
